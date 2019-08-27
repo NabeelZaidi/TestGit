519f228dcec4152c8469918ff5051db607ef5fe8
NabeelZaidi <37018555+NabeelZaidi@users.noreply.github.com>
2019-08-27 17:22:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,8 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "Hello World!"
-
+@app.route("/hello")
+def hello():
+    return "{'name':'nabeel'}"
 if __name__ == '__main__':
     app.run()
